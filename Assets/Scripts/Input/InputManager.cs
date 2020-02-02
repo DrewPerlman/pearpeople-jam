61a7fe2caa420b5d2ab96a25f048a6e2e047fb80
Swap <sp70215@gmail.com>
2020-02-02 15:02:25 -0800

    Add keyboard input to game
    Contained in LevelScene-1 scene
    Adjusted PaddleController to work with keyboard input

---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class InputManager : MonoBehaviour {
+    public float Direction { get; protected set; }
+    public bool StartPressed { get; protected set; }
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        CheckInputs();
+    }
+
+    protected abstract void CheckInputs();
+}
---
+++ Assets/Scripts/Input/KeyboardInputManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardInputManager : InputManager {
+    [SerializeField] private KeyCode moveLeft = KeyCode.None;
+    [SerializeField] private KeyCode moveRight = KeyCode.None;
+    [SerializeField] private KeyCode startButton = KeyCode.None;
+
+    // On start, check that the KeyCodes are set 
+    private void Start() {
+        if (moveLeft == KeyCode.None || moveRight == KeyCode.None
+            || startButton == KeyCode.None) {
+            Debug.Log("Controls not fully mapped!");
+        }
+    }
+
+    override protected void CheckInputs() {
+        if (Input.GetKey(moveLeft) && Input.GetKey(moveRight)) {
+            Direction = 0;
+        }
+        else if (Input.GetKey(moveLeft)) {
+            Direction = -1;
+        }
+        else if (Input.GetKey(moveRight)) {
+            Direction = 1;
+        }
+        else {
+            Direction = 0;
+        }
+        StartPressed = Input.GetKey(startButton);
+    }
+}
