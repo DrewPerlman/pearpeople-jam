b2fa4a888d2cf7983093b152c970fb9613df0d80
DALTONS-LAPTOP\Dalton <dchancel@usc.edu>
2020-02-01 22:49:25 -0600

    progress bar
    progress bar grabs every gameobject with tag of "garbage", and increments an image's fill amount every time a clump of garbage is picked up

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+	public static ProgressBar instance;
+
+    private List<GameObject> garbageList = new List<GameObject>();
+    private int garbageCount;
+    [SerializeField]
+    private Image bar;
+
+    private void Awake(){
+    	if(instance == null){
+    		instance = this;
+    	} else{
+    		Destroy(this.gameObject);
+    	}
+    }
+
+    private void Start(){
+    	garbageList.AddRange(GameObject.FindGameObjectsWithTag("Garbage"));
+    	garbageCount = garbageList.Count;
+    	float amt = 1 - garbageList.Count/garbageCount;
+    	print(amt.ToString());
+    	UpdateBar();
+    }
+
+    public void RemoveEntry(GameObject entry){
+    	garbageList.Remove(entry);
+    	UpdateBar();
+    }
+
+    private void UpdateBar(){
+    	bar.fillAmount = 1f - (float)garbageList.Count/(float)garbageCount;
+    }
+}
