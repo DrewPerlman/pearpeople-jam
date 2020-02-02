11383e1ba4c92293496af3e2bd098866a5304932
Drew Perlman <drew.perlman@gmail.com>
2020-02-02 12:49:00 -0800

    random garbage spawning

---
+++ Assets/Scripts/GarbageSpawning.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarbageSpawning : MonoBehaviour
+{
+	public GameObject garbagePrefab;
+	public int numGarbage;
+
+	private void Start()
+	{
+		for (int i = 0; i < numGarbage; i++)
+		{
+			spawnGarbage();
+		}
+	}
+
+	void spawnGarbage()
+	{
+		Vector2 someObjectPosition = new Vector2(0, -0.54f);
+		Vector2 newPosition = someObjectPosition + Random.insideUnitCircle * 3.8f;
+		GameObject garbageInstance = Instantiate(garbagePrefab, newPosition, Quaternion.identity);
+	}
+}
