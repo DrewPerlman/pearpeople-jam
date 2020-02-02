e83ba336f82bead99f764b07339b1955ca4d9e3d
glacay30 <glacayo@usc.edu>
2020-02-02 14:52:21 -0800

    Create SceneRefresher
    Press R to refresh the current scene.

---
+++ Assets/Scripts/Util/SceneRefresher.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneRefresher : MonoBehaviour
+{
+    [SerializeField] private KeyCode refreshKey = KeyCode.R;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(refreshKey)) {
+            int activeSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(activeSceneIndex);
+        }
+    }
+}
