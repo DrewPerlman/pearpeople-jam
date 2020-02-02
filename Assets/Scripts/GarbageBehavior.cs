b9821e9527325702882c93ef9003c0254ce4adab
Drew Perlman <drew.perlman@gmail.com>
2020-02-01 17:27:11 -0800

    add vacuum

---
+++ Assets/Scripts/GarbageBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarbageBehavior : MonoBehaviour
+{
+	public bool movingTowardsVacuum = false;
+	public float moveTowardsSpeed = .5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (movingTowardsVacuum)
+		{
+			moveTowardsVacuum();
+		}
+    }
+
+	private void moveTowardsVacuum()
+	{
+		print("I'M Moving");
+		Vector2 target = FindObjectOfType<VacuumBehavior>().transform.position;
+		transform.position = Vector2.MoveTowards(transform.position, target, moveTowardsSpeed);
+		if (Vector2.Distance(this.transform.position, target) < 0.2f)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/VacuumBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VacuumBehavior : MonoBehaviour
+{
+	public float vicinityRadius = 1f;
+	public GameObject spriteMaskPrefab;
+	public GameObject spriteMaskParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		InvokeRepeating("CreateDirtLayerSpriteMask", 0f, .1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+	}
+
+	private void OnTriggerEnter2D (Collider2D collision)
+	{
+		if (collision.gameObject.tag == "Garbage")
+		{
+			collision.GetComponent<GarbageBehavior>().movingTowardsVacuum = true;
+		}
+	}
+
+	private void CreateDirtLayerSpriteMask()
+	{
+		print("CREATING INSTNACE");
+		GameObject spriteInstance = Instantiate(spriteMaskPrefab, this.transform.position, Quaternion.identity);
+		spriteInstance.transform.parent = spriteMaskParent.transform;
+	}
+}
